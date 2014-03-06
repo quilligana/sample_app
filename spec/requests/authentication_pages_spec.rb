9c3d7df3bda8e183d441a241d52aecc8a72c022a
Aidan Quilligan <a.quilligan@gmail.com>
2014-03-06 23:42:11 +0000

    Added follow/unfollow buttons

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -73,9 +73,9 @@ describe "Authentication" do
 			            it "should render the default (profile) page" do
 			              expect(page).to have_title(user.name)
 			            end
-			      end
+				      end
 		        end
-          end
+        end
 
 	      describe "in the Users controller" do
 
@@ -105,6 +105,18 @@ describe "Authentication" do
 		        end
 	      end
 	    end
+
+	    describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
 
 	    describe "in the Microposts controller" do
 
