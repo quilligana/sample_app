0064b03fa303bd32374df159ec28cc9ff6a8a62f
Aidan Quilligan <a.quilligan@gmail.com>
2014-03-10 00:01:55 +0000

    Chapter 11 exercises 1 & 2

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -5,6 +5,7 @@ describe Relationship do
   let(:follower) { FactoryGirl.create(:user) }
   let(:followed) { FactoryGirl.create(:user) }
   let(:relationship) { follower.relationships.build(followed_id: followed.id) }
+  
 
   subject { relationship }
 
@@ -25,6 +26,25 @@ describe Relationship do
   describe "when follower id is not present" do
     before { relationship.follower_id = nil }
     it { should_not be_valid }
+  end
+
+  describe "when a user is deleted" do
+    before do
+      follower.save!
+      followed.save!
+      relationship.save!
+    end
+    it "should destroy associated relationships" do
+      relationships = follower.relationships.to_a
+      expect(relationships).not_to be_empty
+      followed.destroy!
+      relationships.each do |relationship|
+        expect(Relationship.where(id: relationship.id)).to be_empty
+      end
+      # expect(User.where(id: relationship.followed_id).to be_empty)
+      # expect(relationships).not_to be_empty
+      # expect(relationship).to be_nil
+    end
   end
 
 end
